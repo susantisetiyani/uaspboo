26bf93292206005fe3f7bbcc870544a453d64ef9
fared1 <58796383+fared1@users.noreply.github.com>
2020-01-10 00:17:37 +0700

    Add files via upload

---
+++ MySQL.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package setting;
+
+/**
+ *
+ * @author Naubyra
+ */
+public interface MySQL {
+    public String dbHost = "localhost";
+    public String dbName = "perpusuas";
+    public String dbUser = "root";
+    public String dbPass = "";
+}
