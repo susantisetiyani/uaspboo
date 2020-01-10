d93bd814216dd1cad0b746c8edd4a7a9e4296285
fared1 <58796383+fared1@users.noreply.github.com>
2020-01-10 09:00:25 +0700

    Add files via upload

---
+++ src/dbcrud/CRUD.java
@@ -0,0 +1,22 @@
+package dbcrud;
+
+import dbconnect.MySQLConnection;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+/**
+ *
+ * @author hp
+ * @param <MySQLConnection>
+ */
+public abstract class CRUD<MySQLConnection> {
+    public abstract void insert(MySQLConnection m);
+    public abstract void delete(MySQLConnection m);
+    public abstract void update(MySQLConnection m);
+    public abstract void select(MySQLConnection m);
+}
